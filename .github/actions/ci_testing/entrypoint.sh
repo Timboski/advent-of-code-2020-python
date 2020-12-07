d3f37a89258c30cd5e5728df07e608f3377aa696
Tim Boyle <timboboyle@yahoo.co.uk>
2020-12-07 23:41:08 +0000

    Fail if CI steps fail

--- .github/actions/ci_testing/entrypoint.sh
+++ .github/actions/ci_testing/entrypoint.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 echo "Run the unit tests"
-python -m unittest discover -v test
+if ! python -m unittest discover -v test; then
+  echo "Error: Unit Tests Failed"
+  exit 1
+fi
 
 echo "Checking flake8 compliance"
-flake8 advent2020 test
+if ! flake8 advent2020 test; then
+  echo "Error: Code fails flake8 compliance"
+  exit 2
+fi
 
 echo "Checking yapf formatting"
-yapf -d -r advent2020 test
+if ! yapf -d -r advent2020 test; then
+  echo "Error: Code not yapf formatted correctly"
+  exit 3
+fi
