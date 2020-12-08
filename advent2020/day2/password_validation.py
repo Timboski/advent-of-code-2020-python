af70975c0cf259a69412c7aaec696baa6cc0730b
Tim Boyle <tim.boyle@oxionics.com>
2020-12-08 22:57:32 +0000

    Read list of entries from file

--- advent2020/day2/password_validation.py
+++ advent2020/day2/password_validation.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from advent2020.day2.password_entry import PasswordEntry
 
 
@@ -6,3 +8,11 @@ class PasswordValidation:
         p = PasswordEntry(entry)
         occurances = p.password.count(p.letter)
         return (occurances >= p.min) and (occurances <= p.max)
+
+    def read_report(self, path: str) -> List[int]:
+        with open(path) as stream:
+            return [entry.strip() for entry in list(stream)]
+
+
+if __name__ == "__main__":
+    pass
--- test/day2/test_password_validation.py
+++ test/day2/test_password_validation.py
@@ -35,3 +35,15 @@ class TestPasswordValidation(unittest.TestCase):
 
         # Assert
         self.assertTrue(result)
+        
+    def test_read_file_to_a_list(self):
+        # Arrange
+        example_data = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+        example_data_path = "test/day2/example_data"
+        sut = PasswordValidation()
+
+        # Act
+        data = sut.read_report(example_data_path)
+
+        # Assert
+        self.assertSequenceEqual(data, example_data)
