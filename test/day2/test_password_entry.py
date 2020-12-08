3035a3e25ff3e2994685defe4d2469f7c14c8b0f
Tim Boyle <tim.boyle@oxionics.com>
2020-12-08 22:44:26 +0000

    Split long line (yapf)

--- test/day2/test_password_entry.py
+++ test/day2/test_password_entry.py
@@ -18,7 +18,8 @@ class TestPasswordEntry(unittest.TestCase):
         self.assertEqual(sut.letter, "a")
         self.assertEqual(sut.password, "abcde")
 
-    @parameterized.expand([("1-3 a: abcde",), ("1-3 b: cdefg",), ("2-9 c: ccccccccc",)])
+    @parameterized.expand([("1-3 a: abcde", ), ("1-3 b: cdefg", ),
+                           ("2-9 c: ccccccccc", )])
     def test_decompose_examples(self, entry: str):
         # Arrange, Act
         sut = PasswordEntry(entry)
