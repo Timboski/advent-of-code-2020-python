f38c45b439b0a27b982d2930564eb53d5d148541
Tim Boyle <tim.boyle@oxionics.com>
2020-12-10 23:06:52 +0000

    Check Puzzle 2 example total

--- test/day3/test_route.py
+++ test/day3/test_route.py
@@ -26,13 +26,12 @@ class TestRoute(unittest.TestCase):
         # Assert
         self.assertEqual(trees_encountered, 171)
 
-    def test_example_puzzle2(self):
+    def test_example_puzzle2_routes(self):
         # Arrange
         example_input = "test/day3/example_input"
         sut = Route(example_input)
 
         # Act
-        trees_encountered = []
         trees_encountered = [sut.toboggan_run(1, 1)]  # Right 1, down 1.
         trees_encountered.append(sut.toboggan_run(3, 1))  # Right 3, down 1.
         trees_encountered.append(sut.toboggan_run(5, 1))  # Right 5, down 1.
@@ -41,3 +40,18 @@ class TestRoute(unittest.TestCase):
 
         # Assert
         self.assertSequenceEqual(trees_encountered, [2, 7, 3, 4, 2])
+
+    def test_example_puzzle2_total(self):
+        # Arrange
+        example_input = "test/day3/example_input"
+        sut = Route(example_input)
+
+        # Act
+        trees_encountered = sut.toboggan_run(1, 1)  # Right 1, down 1.
+        trees_encountered *= sut.toboggan_run(3, 1)  # Right 3, down 1.
+        trees_encountered *= sut.toboggan_run(5, 1)  # Right 5, down 1.
+        trees_encountered *= sut.toboggan_run(7, 1)  # Right 7, down 1.
+        trees_encountered *= sut.toboggan_run(1, 2)  # Right 1, down 2.
+
+        # Assert
+        self.assertEqual(trees_encountered, 336)
