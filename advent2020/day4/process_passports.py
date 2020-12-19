9763c58265ed60861b1ac1908688b7d40966f9eb
Tim Boyle <tim.boyle@oxionics.com>
2020-12-19 20:52:22 +0000

    Add initial day4 boilerplate

---
+++ advent2020/day4/process_passports.py
@@ -0,0 +1,3 @@
+class ProcessPassports:
+    def __init__(self) -> None:
+        pass
---
+++ test/day4/test_process_passports.py
@@ -0,0 +1,13 @@
+import unittest
+
+from advent2020.day4.process_passports import ProcessPassports
+
+
+class TestProcessPassports(unittest.TestCase):
+    def test_example_puzzle2_routes(self):
+        # Arrange
+        # Act
+        ProcessPassports()
+
+        # Assert
+        self.assertFalse(True)
