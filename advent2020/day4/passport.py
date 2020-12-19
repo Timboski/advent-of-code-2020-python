8004e45ce9188f1b63f6e2d83445bcc02944f535
Tim Boyle <tim.boyle@oxionics.com>
2020-12-19 22:29:55 +0000

    Add dummy validation for all fields

--- advent2020/day4/passport.py
+++ advent2020/day4/passport.py
@@ -2,18 +2,8 @@ from collections import deque
 
 
 class Passport:
-    REQUIRED_FIELDS = [
-        "byr",  # Birth Year
-        "iyr",  # Issue Year
-        "eyr",  # Expiration Year
-        "hgt",  # Height
-        "hcl",  # Hair Color
-        "ecl",  # Eye Color
-        "pid",  # Passport ID
-        # "cid"   # Country ID
-    ]
-
     def __init__(self, passport_entries: deque) -> None:
+        self._field_validation_map = self._create_field_validation_map()
         self._fields = {}
         while passport_entries:
             entry = passport_entries.pop()
@@ -22,8 +12,12 @@ class Passport:
             self._add_entry(entry)
 
     def validate(self) -> bool:
-        for required_field in self.REQUIRED_FIELDS:
+        for required_field in self._field_validation_map:
             if required_field not in self._fields:
+                return False
+            validation = self._field_validation_map[required_field]
+            field_value = self._fields[required_field]
+            if not validation(field_value):
                 return False
         return True
 
@@ -32,3 +26,36 @@ class Passport:
         for field in fields:
             (key, value) = field.split(":")
             self._fields[key] = value
+
+    def _create_field_validation_map(self) -> dict:
+        return {
+            "byr": self._validate_birth_year,
+            "iyr": self._validate_issue_year,
+            "eyr": self._validate_expiration_year,
+            "hgt": self._validate_height,
+            "hcl": self._validate_hair_color,
+            "ecl": self._validate_eye_color,
+            "pid": self._validate_passport_id,
+            # "cid"   # Country ID
+        }
+
+    def _validate_birth_year(sel, entry: str) -> bool:
+        return True
+
+    def _validate_issue_year(self, entry: str) -> bool:
+        return True
+
+    def _validate_expiration_year(self, entry: str) -> bool:
+        return True
+
+    def _validate_height(self, entry: str) -> bool:
+        return True
+
+    def _validate_hair_color(self, entry: str) -> bool:
+        return True
+
+    def _validate_eye_color(self, entry: str) -> bool:
+        return True
+
+    def _validate_passport_id(self, entry: str) -> bool:
+        return True
