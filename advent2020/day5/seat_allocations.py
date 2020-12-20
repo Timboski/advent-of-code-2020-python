391b4b4062271469395767cf274d93a42d0dea41
Tim Boyle <tim.boyle@oxionics.com>
2020-12-20 23:06:59 +0000

    Solve Puzzle 2 in naive way

--- advent2020/day5/seat_allocations.py
+++ advent2020/day5/seat_allocations.py
@@ -8,3 +8,12 @@ class SeatAllocations:
 
     def find_max_occupied(self) -> int:
         return max([seat.id for seat in self._seats])
+
+    def find_empty_seat(self) -> int:
+        id = [seat.id for seat in self._seats]
+        start = min(id)
+        stop = max(id)
+        for seat in range(start, stop):
+            if seat not in id:
+                return seat
+        raise ValueError("Seat not found")
