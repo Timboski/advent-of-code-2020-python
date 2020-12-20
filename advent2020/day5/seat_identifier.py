79a80f644c864925780faa3124e9f9df9aa3c48c
Tim Boyle <tim.boyle@oxionics.com>
2020-12-20 21:40:26 +0000

    Decode column

--- advent2020/day5/seat_identifier.py
+++ advent2020/day5/seat_identifier.py
@@ -4,6 +4,14 @@ class SeatIdentifier:
         self._row = self._row.replace("B", "1")
         self._row = self._row[0:7]
 
+        self._column = seat_id.replace("L", "0")
+        self._column = self._column.replace("R", "1")
+        self._column = self._column[7:10]
+
     @property
     def row(self) -> int:
         return int(self._row, 2)
+
+    @property
+    def column(self) -> int:
+        return int(self._column, 2)
--- test/day5/test_seat_identifier.py
+++ test/day5/test_seat_identifier.py
@@ -1,7 +1,7 @@
 import unittest
-from parameterized import parameterized
 
 from advent2020.day5.seat_identifier import SeatIdentifier
+from parameterized import parameterized
 
 
 class TestSeatIdentifier(unittest.TestCase):
@@ -10,11 +10,19 @@ class TestSeatIdentifier(unittest.TestCase):
     FFFBBBFRRR: row 14, column 7, seat ID 119.
     BBFFBBFRLL: row 102, column 4, seat ID 820.
     '''
-    @parameterized.expand([("BFFFBBFRRR", 70), ("FFFBBBFRRR", 14),
-                           ("BBFFBBFRLL", 102)])
+    @parameterized.expand([("BFFFBBFRRR", 70), ("FFFBBBFRRR", 14), ("BBFFBBFRLL", 102)])
     def test_seat_identifier_return_expected_row(self, seat_id: str, expected_row: int):
         # Arrange, Act
         sut = SeatIdentifier(seat_id)
 
         # Assert
         self.assertEqual(sut.row, expected_row)
+
+    @parameterized.expand([("BFFFBBFRRR", 7), ("FFFBBBFRRR", 7), ("BBFFBBFRLL", 4)])
+    def test_seat_identifier_return_expected_column(self, seat_id: str,
+                                                    expected_column: int):
+        # Arrange, Act
+        sut = SeatIdentifier(seat_id)
+
+        # Assert
+        self.assertEqual(sut.column, expected_column)
