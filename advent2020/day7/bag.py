bde88555375d626aba9e906ae0eb2f75f12bea1b
Tim Boyle <timboboyle@yahoo.co.uk>
2021-10-04 12:43:03 +0000

    Check bag contents

--- advent2020/day7/bag.py
+++ advent2020/day7/bag.py
@@ -1,10 +1,17 @@
-from typing import Set
+from __future__ import annotations
+
+from typing import Dict, Set
 
 
 class Bag:
     def __init__(self, rule: str) -> None:
         self.colour, rules = rule.split(" bags contain ")
         self.contents = Bag._process_bag(rules)
+
+    def contains(self, colour: str, bags: Dict[str, Bag]) -> bool:
+        if any([c == colour for c in self.contents]):
+            return True
+        return any([bags[c].contains(colour, bags) for c in self.contents])
 
     @staticmethod
     def _process_bag(rules: str) -> Set[str]:
--- advent2020/day7/haversacks.py
+++ advent2020/day7/haversacks.py
@@ -1,9 +1,12 @@
+from advent2020.day7.bag import Bag
 from advent2020.utils.read_file import ReadFile
 
 
 class Haversacks:
     def __init__(self, path: str) -> None:
-        self._rules = ReadFile.lines(path)
+        bags = [Bag(line) for line in ReadFile.lines(path)]
+        self._bags = {bag.colour: bag for bag in bags}
 
     def num_colour_options(self) -> int:
-        pass
+        return sum(
+            [1 for c in self._bags if self._bags[c].contains("shiny gold", self._bags)])
