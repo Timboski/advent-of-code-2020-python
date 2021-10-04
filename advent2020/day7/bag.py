9a028af67033c18874e9f2c9ba73c869a34097fd
Tim Boyle <timboboyle@yahoo.co.uk>
2021-10-04 11:03:36 +0000

    Parameterise test

--- advent2020/day7/bag.py
+++ advent2020/day7/bag.py
@@ -1,7 +1,16 @@
+from typing import Set
+
+
 class Bag:
     def __init__(self, rule: str) -> None:
         self.colour, rules = rule.split(" bags contain ")
-        self.contents = {Bag._process_rule(r) for r in rules.split(",")}
+        self.contents = Bag._process_bag(rules)
+
+    @staticmethod
+    def _process_bag(rules: str) -> Set[str]:
+        if "no other " in rules:
+            return set()
+        return {Bag._process_rule(r) for r in rules.split(",")}
 
     @staticmethod
     def _process_rule(rule: str) -> str:
--- test/day7/test_bag.py
+++ test/day7/test_bag.py
@@ -1,17 +1,38 @@
 import unittest
+from typing import Set
 
 from advent2020.day7.bag import Bag
+from parameterized import parameterized
 
 
 class TestBag(unittest.TestCase):
-    def test_construction_from_rule_definition(self):
+    @parameterized.expand([
+        ("light red", "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+         {"bright white", "muted yellow"}),
+        ("dark orange",
+         "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+         {"bright white", "muted yellow"}),
+        ("bright white", "bright white bags contain 1 shiny gold bag.", {"shiny gold"}),
+        ("muted yellow",
+         "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+         {"shiny gold", "faded blue"}),
+        ("shiny gold", "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+         {"dark olive", "vibrant plum"}),
+        ("dark olive",
+         "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+         {"faded blue", "dotted black"}),
+        ("vibrant plum",
+         "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+         {"faded blue", "dotted black"}),
+        ("faded blue", "faded blue bags contain no other bags.", set()),
+        ("dotted black", "dotted black bags contain no other bags.", set()),
+    ])
+    def test_construction_from_rule_definition(self, colour: str, rule: str,
+                                               contents: Set[str]):
         # Arrange
-        rule = "light red bags contain 1 bright white bag, 2 muted yellow bags."
-        expected_contents = {"bright white", "muted yellow"}
-
         # Act
         sut = Bag(rule)
 
         # Assert
-        self.assertEqual(sut.colour, "light red")
-        self.assertSetEqual(sut.contents, expected_contents)
+        self.assertEqual(sut.colour, colour)
+        self.assertSetEqual(sut.contents, contents)
