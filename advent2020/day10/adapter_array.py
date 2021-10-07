edbccce38f9a01186bfa08ef505624adcdf14bd4
Tim Boyle <timboboyle@yahoo.co.uk>
2021-10-07 20:33:29 +0000

    Solve examples

--- advent2020/day10/adapter_array.py
+++ advent2020/day10/adapter_array.py
@@ -3,7 +3,17 @@ from advent2020.utils.read_file import ReadFile
 
 class AdapterArray:
     def __init__(self, path: str) -> None:
-        self._adapters = [int(i) for i in ReadFile.lines(path)]
+        adapters = [int(i) for i in ReadFile.lines(path)]
+        adapters.sort()
+
+        device_jolts = adapters[-1] + 3
+        self._joltages = [0] + adapters + [device_jolts]
 
     def find_sum(self) -> int:
-        pass
+        diffs = [
+            self._joltages[i + 1] - self._joltages[i]
+            for i in range(len(self._joltages) - 1)
+        ]
+        ones = diffs.count(1)
+        threes = diffs.count(3)
+        return ones * threes
