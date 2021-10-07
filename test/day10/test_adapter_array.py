d228a86e731162de4d07df79ef6a826325b1e9d6
Tim Boyle <timboboyle@yahoo.co.uk>
2021-10-07 21:18:28 +0000

    Add regression test for Puzzle 2

--- test/day10/test_adapter_array.py
+++ test/day10/test_adapter_array.py
@@ -19,7 +19,8 @@ class TestAdapterArray(unittest.TestCase):
         self.assertEqual(num, expected_result)
 
     @parameterized.expand([("Example 1", "test/day10/example_input_1", 8),
-                           ("Example 2", "test/day10/example_input_2", 19208)])
+                           ("Example 2", "test/day10/example_input_2", 19208),
+                           ("Regression", "advent2020/day10/input", 32396521357312)])
     def test_puzzle_2(self, test_name: str, path: str, expected_result: int):
         # Arrange
         sut = AdapterArray(path)
