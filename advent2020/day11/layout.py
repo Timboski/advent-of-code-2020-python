8efa3f967d829a5d59c87625a4b871f8d910c84b
Tim Boyle <timboboyle@yahoo.co.uk>
2021-10-08 11:14:46 +0000

    Compare layout states

--- advent2020/day11/layout.py
+++ advent2020/day11/layout.py
@@ -1,14 +1,27 @@
 from __future__ import annotations
 
+from typing import List
+
 from advent2020.utils.read_file import ReadFile
 
 
 class Layout:
-    def __init__(self, path: str) -> None:
-        self._start = ReadFile.lines(path)
+    def __init__(self, state: List[str]) -> None:
+        self._state = state
+
+    @staticmethod
+    def from_file(path: str) -> Layout:
+        return Layout(ReadFile.lines(path))
 
     def is_same_state(self, other: Layout) -> bool:
-        pass
+        if len(self._state) != len(other._state):
+            return False
+
+        for i in range(len(self._state)):
+            if self._state[i] != other._state[i]:
+                return False
+
+        return True
 
     def simulate_round(self) -> Layout:
         pass
--- test/day11/test_layout.py
+++ test/day11/test_layout.py
@@ -5,12 +5,36 @@ from parameterized import parameterized
 
 
 class TestLayout(unittest.TestCase):
+    @parameterized.expand([("example_input"), ("example_round_1")])
+    def test_is_same_state(self, input_file: str):
+        # Arrange
+        test_path = "test/day11/"
+        sut = Layout.from_file(test_path + input_file)
+        other = Layout.from_file(test_path + input_file)
+
+        # Act
+        res = sut.is_same_state(other)
+
+        # Assert
+        self.assertTrue(res)
+
+    def test_not_same_state(self):
+        # Arrange
+        sut = Layout.from_file("test/day11/example_input")
+        other = Layout.from_file("test/day11/example_round_1")
+
+        # Act
+        res = sut.is_same_state(other)
+
+        # Assert
+        self.assertFalse(res)
+
     @parameterized.expand([("example_input", "example_round_1")])
     def test_simulate_round(self, input_file: str, expected_result_file: str):
         # Arrange
         test_path = "test/day11/"
-        sut = Layout(test_path + input_file)
-        expected = Layout(test_path + expected_result_file)
+        sut = Layout.from_file(test_path + input_file)
+        expected = Layout.from_file(test_path + expected_result_file)
 
         # Act
         end = sut.simulate_round()
