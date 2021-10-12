0f65c31b03507e3a82087da72f6b1bacc30ada72
Tim Boyle <timboboyle@yahoo.co.uk>
2021-10-12 13:14:27 +0000

    Solve example puzzle 2

--- advent2020/day13/order.py
+++ advent2020/day13/order.py
@@ -4,4 +4,18 @@ class Order:
                         if x != 'x']
 
     def find_sequence_start_time(self) -> int:
-        pass
+        index_max_time = max(range(len(self._busses)),
+                             key=[bus for _, bus in self._busses].__getitem__)
+        index, step_size = self._busses[index_max_time]
+        time = step_size - index
+        while True:
+            if self._test_sequence(time):
+                return time
+            time += step_size
+
+    def _test_sequence(self, time: int) -> bool:
+        for index, bus in self._busses:
+            target = time + index
+            if target % bus:
+                return False
+        return True
